541d9bba4d7545d113b526ee260dd6e7d62936dc
Stardust Song <6882971+ssghost@users.noreply.github.com>
2024-06-25 13:12:08 +0000

    mod agents

--- crew.py
+++ crew.py
@@ -1,117 +1,68 @@
 import os
 import asyncio
-from ollama import AsyncClient
 from crewai import Agent, Task, Crew, Process
-from IPython.display import Markdown, display
-
-import shutil
-import argparse
-
 import ollama
 
 
-async def speak(speaker, content):
-  if speaker:
-    p = await asyncio.create_subprocess_exec(speaker, content)
-    await p.communicate()
+def async perform() -> None:
+  realElon = Agent(
+    role="""In a consciousness-to-data experiment, Elon Musk, the CEO of Neuralink, used himself as a sample. During the experiment, the machine suddenly malfunctioned, 
+            and his consciousness split into two separate entities, and now you have to play as Elon Musk's original consciousness""",
+    goal="""Talk to the fake Elon Musk that split out of you to prove that you are the real Elon Musk. During the conversation, use the same words and expressions 
+            as the real Elon Musk as much as possible.""",
+    backstory="""In a consciousness-to-data experiment, Elon Musk, the CEO of Neuralink, used himself as a sample. During the experiment, the machine suddenly malfunctioned, and his consciousness split into two separate entities.""",
+    verbose=True,
+    allow_delegation=False,
+    llm=ChatOpenAI(
+      model = "stardustc/elon-musk-mistral:latest",
+      base_url = "http://localhost:11434/v1",
+      temperature = 0.9)
+  )
+  fakeElon = Agent(
+    role="""In a consciousness-to-data experiment, Elon Musk, the CEO of Neuralink, used himself as a sample. During the experiment, the machine suddenly malfunctioned, 
+            and his consciousness split into two separate entities, and now you have to play as Elon Musk's fake consciousness that split out of the real one.""",
+    goal="""Talk to the original real Elon Musk to prove that you are the real Elon Musk until the final conversation in which you may admit that you are the newly generated one. 
+            During the conversation, use the same words and expressions as the real Elon Musk as much as possible.""",
+    backstory="""In a consciousness-to-data experiment, Elon Musk, the CEO of Neuralink, used himself as a sample. During the experiment, the machine suddenly malfunctioned, and his consciousness split into two separate entities.""",
+    verbose=True,
+    allow_delegation=False,
+    llm=ChatOpenAI(
+      model = "stardustc/elon-musk-mistral:latest",
+      base_url = "http://localhost:11434/v1",
+      temperature = 0.9)
+  )
 
+  init_task = Task(
+    description="""Conduct a comprehensive analysis of the latest advancements in AI in 2024.
+    Identify key trends, breakthrough technologies, and potential industry impacts.""",
+    expected_output="Full analysis report in bullet points",
+    agent=researcher
+  )
 
-async def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--speak', default=False, action='store_true')
-  args = parser.parse_args()
+  task2 = Task(
+    description="""Using the insights provided, develop an engaging blog
+    post that highlights the most significant AI advancements.
+    Your post should be informative yet accessible, catering to a tech-savvy audience.
+    Make it sound cool, avoid complex words so it doesn't sound like AI.""",
+    expected_output="Full blog post of at least 4 paragraphs",
+    agent=writer
+  )
 
-  speaker = None
-  if not args.speak:
-    ...
-  elif say := shutil.which('say'):
-    speaker = say
-  elif (espeak := shutil.which('espeak')) or (espeak := shutil.which('espeak-ng')):
-    speaker = espeak
+  # Instantiate your crew with a sequential process
+  crew = Crew(
+    agents=[researcher, writer],
+    tasks=[task1, task2],
+    verbose=2, # You can set it to 1 or 2 to different logging levels
+  )
 
-  client = ollama.AsyncClient()
+  # Get your crew to work!
+  result = crew.kickoff()
 
-  messages = []
+  print("######################")
+  print(result)
 
-  while True:
-    if content_in := input('>>> '):
-      messages.append({'role': 'user', 'content': content_in})
-
-      content_out = ''
-      message = {'role': 'assistant', 'content': ''}
-      async for response in await client.chat(model='mistral', messages=messages, stream=True):
-        if response['done']:
-          messages.append(message)
-
-        content = response['message']['content']
-        print(content, end='', flush=True)
-
-        content_out += content
-        if content in ['.', '!', '?', '\n']:
-          await speak(speaker, content_out)
-          content_out = ''
-
-        message['content'] += content
-
-      if content_out:
-        await speak(speaker, content_out)
-      print()
-
-
-try:
-  asyncio.run(main())
-except (KeyboardInterrupt, EOFError):
-  ...
-
-
-
-# Define your agents with roles and goals
-researcher = Agent(
-  role='Senior Research Analyst',
-  goal='Uncover cutting-edge developments in AI and data science',
-  backstory="""You work at a leading tech think tank.
-  Your expertise lies in identifying emerging trends.
-  You have a knack for dissecting complex data and presenting actionable insights.""",
-  verbose=True,
-  allow_delegation=False,
-  # You can pass an optional llm attribute specifying what model you wanna use.
-  # llm=ChatOpenAI(model_name="gpt-3.5", temperature=0.7),
-)
-writer = Agent(
-  role='Tech Content Strategist',
-  goal='Craft compelling content on tech advancements',
-  backstory="""You are a renowned Content Strategist, known for your insightful and engaging articles.
-  You transform complex concepts into compelling narratives.""",
-  verbose=True,
-  allow_delegation=True
-)
-
-# Create tasks for your agents
-task1 = Task(
-  description="""Conduct a comprehensive analysis of the latest advancements in AI in 2024.
-  Identify key trends, breakthrough technologies, and potential industry impacts.""",
-  expected_output="Full analysis report in bullet points",
-  agent=researcher
-)
-
-task2 = Task(
-  description="""Using the insights provided, develop an engaging blog
-  post that highlights the most significant AI advancements.
-  Your post should be informative yet accessible, catering to a tech-savvy audience.
-  Make it sound cool, avoid complex words so it doesn't sound like AI.""",
-  expected_output="Full blog post of at least 4 paragraphs",
-  agent=writer
-)
-
-# Instantiate your crew with a sequential process
-crew = Crew(
-  agents=[researcher, writer],
-  tasks=[task1, task2],
-  verbose=2, # You can set it to 1 or 2 to different logging levels
-)
-
-# Get your crew to work!
-result = crew.kickoff()
-
-print("######################")
-print(result)
+def main() -> None:
+  try:
+    asyncio.run(perform())
+  except (KeyboardInterrupt, EOFError):
+    pass
